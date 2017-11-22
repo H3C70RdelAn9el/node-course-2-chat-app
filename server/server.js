d03e5e859788df8d928674dc69267f975f26ab88
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-11-22 15:00:45 -0800

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,6 +15,31 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    // socket.emit('newEmail', {
+    //     from: 'mike@example.com',
+    //     text: 'Hey, what\'s going on?',
+    //     createdAt: 123
+    // });
+
+    // socket.on('createEmail', (newEmail) => {
+    //     console.log('createEmail', newEmail);
+    // });
+
+    // socket.emit('newMessage', {
+    //     from: 'barney@example.com',
+    //     text: 'but i love you!',
+    //     createdAt: 123
+    // });
+
+    socket.on('createMessage', (message) =>{
+        console.log('createMessage', message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
+    });
+
     socket.on('disconnect', () => {
         console.log('User was disconnected');
     });
