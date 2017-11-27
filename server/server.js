232ee022aacff999d5cc0941db29e15786fe0927
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-11-27 12:05:01 -0800

    Greet new user, and alert other

--- server/server.js
+++ server/server.js
@@ -31,6 +31,21 @@ io.on('connection', (socket) => {
     //     createdAt: 123
     // });
 
+    //socket.emit from Admin text Welcome to the chat
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+    //socket.broadcastemit from Admin text New user joined
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
+
+
     socket.on('createMessage', (message) =>{
         console.log('createMessage', message);
         io.emit('newMessage', {
@@ -38,6 +53,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+
     });
 
     socket.on('disconnect', () => {
