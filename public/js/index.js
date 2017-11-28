16af80bcfa96d6f97ff4bf583368615f089a6715
H3C70RdelAn9el <H3C70R@H3C70Rs-MBP.local>
2017-11-28 12:50:06 -0800

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -40,6 +40,16 @@ socket.on('newMessage',  function (message) {
 //     console.log('Got it!', data);
 // });
 
+socket.on('newLocationMessage', function (message) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>');
+
+    li.text(`${message.from}:   `); //set persons name and colon
+    a.attr('href', message.url);
+    li.append(a);
+    jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
@@ -48,5 +58,22 @@ jQuery('#message-form').on('submit', function (e) {
         text: jQuery('[name=message]').val()
     }, function () {
 
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser.');
+    }
+
+    //fetching the user location:
+    navigator.geolocation.getCurrentPosition(function (position){
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function () {
+        alert('Unable to fetch location.');
     });
 });
